c608b03225ba9fccfe2edb0aab9ed02372c3b347
Shaikateeqahmed <shaikateeqahmed892@gmail.com>
2023-09-02 17:17:10 +0530

    deploumnet

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.send("HOME PAGE")
+})
 app.use("/data",Data);
 
 app.listen(process.env.Port,async()=>{
